0344db3844547185c2e2dba41238b9ab9818c3fa
naman74 <43387378+naman74@users.noreply.github.com>
2018-11-13 00:17:23 +0530

    Second pattern program

---
+++ src/fourpattern/Fourpattern.java
@@ -0,0 +1,19 @@
+
+package fourpattern;
+
+
+public class Fourpattern {
+
+    
+    public static void main(String[] args) {
+        int n=5,k=4;
+        for(int i=0;i<n;i++){
+            System.out.println("44"+k+"44");
+            if(k==4)
+            {
+                k=0;
+            }
+            k++;
+    }
+    }  
+}
